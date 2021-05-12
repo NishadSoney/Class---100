f4e0a5a42c5d106ac08a52dfadecf00c337c3482
NishadSoney <69256732+NishadSoney@users.noreply.github.com>
2021-05-12 18:20:02 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,19 @@
+class Atm():
+    def __init__(self,card_number,pin):
+        self.card_number = card_number
+        self.pin = pin
+
+    def Withdraw_Cash():
+        print("Withdraw cash here")
+
+    def Check_Balance():
+        print("Check you balance here")
+
+    def Deposit_Money():
+        print("Desposit your money here")
+
+    def Transfer_Money():
+        print("Transfer your Money here")
+
+    def Change_Card_Things():
+        print("Change your card settings here")
